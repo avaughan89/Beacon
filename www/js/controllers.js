0ab315a8a75cb7f854c2b3188dc0d60a082356f8
Andrew Vaughan & Paul <pair+avaughan89.pyu06@devbootcamp.com>
2015-02-14 11:39:03 -0800

    Geolocation has been implemented for homescreen using GMaps support

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -67,6 +67,17 @@ Beacon.controller('MapController', function() {
       lng: -77.028333,
       draggable: false
 })
+    GMaps.geolocate({
+      success: function(position) {
+        map.setCenter(position.coords.latitude, position.coords.longitude);
+      },
+      error: function(error) {
+        alert('Geolocation failed' + error.message);
+      },
+      not_supported: function() {
+        alert('Your browser does not support Geolocation')
+      }
+    })
 
         // $scope.map = map;
 
